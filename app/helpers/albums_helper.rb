8ff02602552c11d3f6c1338b4ae078cef4b62ed0
Matt McKenzie <matthew.mitchell.mckenzie@gmail.com>
2019-07-12 15:23:58 +1000

    hours and minutes helpers added

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -6,10 +6,16 @@ module AlbumsHelper
         end
     end
 
-    def albums_helper(minutes)
+    def minutes_helper(minutes)
         hours = minutes / 60
         minutes_remaining = minutes % 60
-        result = "#{hours}:#{minutes_remaining}"
+        if hours < 1
+            return "#{minutes_remaining} minutes"
+        elsif hours == 1 
+            return "#{hours} hour #{minutes_remaining} minutes"
+        else
+            return "#{hours} hours #{minutes_remaining} minutes"
+        end
     end
 
 end
